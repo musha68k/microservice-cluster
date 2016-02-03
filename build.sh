f0953d954931c22781ef63681d4d5c5cf97ecb9b
Oskar M. Grande <oskar@daruma.io>
2016-02-03 17:37:41 +0100

    Reference correct relative path for environment specific docker files

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@
 
 log_status() {
   echo ""
-  echo "---------------------------------------"
-  echo "# Creating $1 container image"
-  echo "---------------------------------------"
+  echo "# Creating \"$1\" container image"
+  echo ""
+}
+
+log_error() {
+  echo ""
+  echo "# Build failed: $1"
   echo ""
 }
 
@@ -16,7 +20,7 @@ for BASE_SERVICE_DIR in services/base/*; do
   BASE_SERVICE=$(basename "$BASE_SERVICE_DIR")
 
   log_status "$BASE_SERVICE"
-  docker build -t "${BASE_SERVICE}-build" "$BASE_SERVICE_DIR"
+  docker build -t "${BASE_SERVICE}-build" -f "${BASE_SERVICE_DIR}/Dockerfile.build" "$BASE_SERVICE_DIR"
 
 done
 
@@ -26,15 +30,15 @@ for APP_SERVICE_DIR in services/app/*; do
   APP_SERVICE=$(basename "$APP_SERVICE_DIR")
 
   log_status "$APP_SERVICE"
-  docker build -t "{$APP_SERVICE}-build" -f Dockerfile "$APP_SERVICE_DIR"
+  docker build -t "{$APP_SERVICE}-build" -f "${APP_SERVICE_DIR}/Dockerfile.build" "$APP_SERVICE_DIR"
 
   if [ "$ENV" = "development" ]; then
-    docker build -t "{$APP_SERVICE}-development" -f Dockerfile.development "$APP_SERVICE_DIR"
+    docker build -t "{$APP_SERVICE}-development" -f "${APP_SERVICE_DIR}/Dockerfile.development" "$APP_SERVICE_DIR"
   elif [ "$ENV" = "production" ]; then
-    docker build -t "${APP_SERVICE}-production" -f Dockerfile.production "$APP_SERVICE_DIR"
+    docker build -t "${APP_SERVICE}-production" -f "${APP_SERVICE_DIR}/Dockerfile.production" "$APP_SERVICE_DIR"
 
   else
-    echo "Environment must be either one of 'development' or 'production'."
+    log_error "Environment must be either one of 'development' or 'production'"
     break
 
   fi
